9229322910ce6487e353688027aaabb6d8bd2717
Lynette Abney <shglynette321@yahoo.com>
2020-02-21 02:04:34 +0000

    Automatically backed up by Learn

--- lib/board.rb
+++ lib/board.rb
@@ -1,6 +1,12 @@
-# Define the variable board below.
 board = board
-board = array
-row 1 = [" ", " ", " "]
-row 2 = [" ", " ", " "]
-row 3 = [" ", " ", " "]
+
+board = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+row = [" ", " ", " "]
+# Position 1: left
+row[0]
+
+# Position 2: Middle
+row[1]
+
+#Position 3: right
+row[2]
